e2806beb8f9f1d6ac14a04cd739f51479e3e9e5c
Kimjuseong <k021229@naver.com>
2020-06-26 17:04:55 +0900

    DAL-3 [ADD] 로그인 상태 헤더 추가

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Header.css';
 import DalentLogo from '../../Images/dalent.png';
+import Ranker from '../../Images/ranker.png';
 function Header() {
     return (
         <div class="dalent-header">
+            
             <div class="header-container">
+                
                 <a href="#"><img class="dalent-logo" src={DalentLogo}/></a>
-                <a href="/login"><div class="login-text">로그인</div></a>
+                {/*비로그인 상태
+                a href="/login"><div class="login-text">로그인</div></a>*/}
+
+                {/*로그인 상태*/}
+                <div class="login-wrapper">
+                    <a href="#"><div class="add-work">작품등록</div></a>
+                    <div class="user-info">
+                        <div class="user-name">Sun_Gun</div>
+                        <img class="user-profile" src={Ranker}/>
+                    </div>
+                </div>
             </div>
         </div>
     );
