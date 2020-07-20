80c7d1d2cd100e214285414bdefe1e27bbc48603
Kimjuseong <k021229@naver.com>
2020-07-20 15:03:16 +0900

    Updates

---
+++ precache-manifest.a4b0c0e0f1849454aaf62b4fabf8ed2b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153f0fe8eda02218129cbcc95a4eb921",
+    "url": "/Dalent-Frontend/index.html"
+  },
+  {
+    "revision": "fa331779ed69f8fa1c7b",
+    "url": "/Dalent-Frontend/static/css/main.b3e0be5e.chunk.css"
+  },
+  {
+    "revision": "d1bb2309de842eb6a490",
+    "url": "/Dalent-Frontend/static/js/2.a00e875e.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/Dalent-Frontend/static/js/2.a00e875e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa331779ed69f8fa1c7b",
+    "url": "/Dalent-Frontend/static/js/main.8bd8d578.chunk.js"
+  },
+  {
+    "revision": "ab7d1f4010c421301b1b",
+    "url": "/Dalent-Frontend/static/js/runtime-main.12e4e17c.js"
+  },
+  {
+    "revision": "f8c5ad9c44ba02743032af121c284f35",
+    "url": "/Dalent-Frontend/static/media/background.f8c5ad9c.png"
+  },
+  {
+    "revision": "3fab96ae44b80ee03fce14af2681ea10",
+    "url": "/Dalent-Frontend/static/media/post.3fab96ae.png"
+  },
+  {
+    "revision": "c1707cd1ebe23ace5b1e1b017e93bae5",
+    "url": "/Dalent-Frontend/static/media/profile.c1707cd1.png"
+  }
+]);
